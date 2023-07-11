772092aa3cc7de33c93b8801e62f821bd5ec7a62
Kseniia <119055649+gimmegummies@users.noreply.github.com>
2023-07-11 17:40:28 +0200

    added Home component for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import './App.css'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserProfile from "./pages/UserProfile.tsx";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home.tsx";
 
 import Header from "./shared/Header/Header.tsx";
 
 function App() {
-
-    return (
-        <Router>
-            <Header/>
-            <Routes>
-                <Route path="/user-profile/:id" element={<UserProfile/>}/>
-            </Routes>
-        </Router>
-    )
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/user-profile/:id" element={<UserProfile />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1>This is a homepage</h1>
+    </div>
+  );
+}
