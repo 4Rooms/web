87d599c41b9ee0b2f862dad46bf530aa7d4c08e7
gimmegummies <black.n.yellow@gmail.com>
2023-07-21 14:41:14 +0200

    clean up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,16 +5,9 @@ import axios from "axios";
 export default function Home() {
   const { username } = useContext(AuthContext);
 
-  const getImg = () => {
-    axios
-      .get("https://prod-chat.duckdns.org/api/profile/avatar/")
-      .then((res) => console.log(res));
-  };
-
   return (
     <div className="Home">
       <h1>This is a homepage, {username}</h1>
-      <button onClick={getImg}>Get img</button>
     </div>
   );
 }
