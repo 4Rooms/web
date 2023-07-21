1136c87b645a4239dde6efa180e33b7aed8bfea9
gimmegummies <black.n.yellow@gmail.com>
2023-07-21 14:43:18 +0200

    clean up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../pages/auth/AuthContext/AuthContext";
-import axios from "axios";
 
 export default function Home() {
   const { username } = useContext(AuthContext);
