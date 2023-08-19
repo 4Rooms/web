3550e83d9db9886a598d31eb3d23028aa52e5776
denis <waridesu@gmail.com>
2023-08-19 16:22:56 +0300

    remove import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useEffect, useContext } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
