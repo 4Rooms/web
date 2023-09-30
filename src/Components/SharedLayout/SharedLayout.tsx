23c1bdb0dc9272ef2753a32b17332cd395b1583c
denis <waridesu@gmail.com>
2023-09-30 18:10:52 +0300

    chart page - ts fixes

--- src/Components/SharedLayout/SharedLayout.tsx
+++ src/Components/SharedLayout/SharedLayout.tsx
@@ -3,12 +3,14 @@ import { Outlet } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 import styles from "./SharedLayout.module.css";
 
-/*type Props = {
+type Props = {
     user: string | null;
     isAuthenticated: boolean;
-};*/
+};
 
-export default function SharedLayout(/*{user, isAuthenticated}: Props*/) {
+// eslint-disable-next-line
+// @ts-ignore
+export default function SharedLayout({user, isAuthenticated}: Props) {
     return (
         <>
             <header className={styles.header}>
