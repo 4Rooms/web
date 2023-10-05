34349e688c9ecaaa37013e8b3153b5f08df53fb4
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-10-06 01:47:39 +0300

    add choose dashboard

--- src/pages/dashboard/Dassboard.tsx
+++ src/pages/dashboard/Dassboard.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import isPropValid from "@emotion/is-prop-valid";
 import styles from "./Dashboard.module.css";
 import { optionDashboard } from "../../utils/optionDashboard";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export function DashboardPage() {
+    interface StyledLiProps {
+        hoverItem: string;
+        backGround: string;
+    }
+
+    const Item = styled.li.withConfig({
+        shouldForwardProp: (prop) =>
+            isPropValid(prop) && prop !== "hoverItem" && prop !== "backGround",
+    })<StyledLiProps>`
+        background-color: ${({ backGround }) => backGround && backGround};
+        &:hover {
+            background-color: ${({ hoverItem }) => hoverItem && hoverItem};
+        }
+    `;
     return (
         <div className={styles.overlay}>
             <div>
@@ -12,21 +28,35 @@ export function DashboardPage() {
                 </h1>
                 <ul className={styles.list__dashboard}>
                     {optionDashboard.map((option) => {
+                        console.log(option.background);
                         return (
-                            <li
-                                style={{ backgroundColor: option.background }}
+                            <Item
                                 className={styles.item__dashboard}
+                                key={option.name}
+                                hoverItem={option.hover}
+                                backGround={option.background}
                             >
                                 <Link
                                     to={option.name}
                                     style={{ width: "100%" }}
-                                    className={styles.item__dashboard}
+                                    className={styles.link__dashboard}
                                 >
-                                    <p className={styles.text__dashboard}>
-                                        {option.name}
-                                    </p>
+                                    <div>
+                                        <p className={styles.text__dashboard}>
+                                            {option.name}
+                                        </p>
+                                        <p
+                                            className={
+                                                styles.text__dashboard__desctiption
+                                            }
+                                        >
+                                            You can chat about any movies or
+                                            series you've already watched or
+                                            plan to watch.
+                                        </p>
+                                    </div>
                                 </Link>
-                            </li>
+                            </Item>
                         );
                     })}
                 </ul>
--- src/utils/optionDashboard.ts
+++ src/utils/optionDashboard.ts
@@ -1,18 +1,22 @@
-export const optionDashboard: { name: string; background: string }[] = [
+export const optionDashboard: { name: string; background: string, hover: string }[] = [
     {
         name: "Cinema",
         background: "#B3DCCF",
+        hover: "#61C9A8",
     },
     {
         name: "Games",
         background: "#F2B5B5",
+        hover: "#F66",
     },
     {
         name: "Books",
         background: "#EED78C",
+        hover: "#F5BB00",
     },
     {
         name: "Music",
         background: "#DFB8F1",
+        hover: "#D06EFD",
     },
 ];
