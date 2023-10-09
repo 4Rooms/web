b8ae7c837d9b2c12808be987152ebd18ccb657db
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-10-10 01:57:47 +0300

    add online user

--- src/pages/Chats/ChatGroup/Online/Online.tsx
+++ src/pages/Chats/ChatGroup/Online/Online.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "./Online.module.css";
+import { CloseModal } from "../../../../assets/icons";
 
 export default function Online() {
     const users: string[] = [
@@ -44,20 +45,42 @@ export default function Online() {
                 ....and 96 others
             </button>
             {open && (
-                <div className={styles.online__list__container}>
-                    <p className={styles.text__online__show}>Online now:</p>
-                    <ul className={styles.online__list}>
-                        {users.map((_, index) => {
-                            return (
-                                <li key={index}>
-                                    <img
-                                        className={styles.online__avatar}
-                                        src="https://assets.nick.com/uri/mgid:arc:imageassetref:shared.nick.us:a625d441-bbbf-42c8-9927-6a0157aac911?quality=0.7&gen=ntrn&legacyStatusCode=true"
-                                    />
-                                </li>
-                            );
-                        })}
-                    </ul>
+                <div className={styles.wrapper__online__show}>
+                    <div className={styles.container__online__show}>
+                        <div className={styles.wrapper__text__show}>
+                            <p className={styles.text__online__show}>
+                                Online now:
+                            </p>
+                            <button
+                                className={styles.button__online__show}
+                                onClick={onClickChangeOpen}
+                            >
+                                <CloseModal />
+                            </button>
+                        </div>
+                        <ul className={styles.list__online__show}>
+                            {users.map((user, index) => {
+                                return (
+                                    <li
+                                        className={styles.item__online__show}
+                                        key={index}
+                                    >
+                                        <img
+                                            className={styles.online__avatar}
+                                            src="https://assets.nick.com/uri/mgid:arc:imageassetref:shared.nick.us:a625d441-bbbf-42c8-9927-6a0157aac911?quality=0.7&gen=ntrn&legacyStatusCode=true"
+                                        />
+                                        <p
+                                            className={
+                                                styles.name__online__show
+                                            }
+                                        >
+                                            {user}
+                                        </p>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
                 </div>
             )}
         </div>
