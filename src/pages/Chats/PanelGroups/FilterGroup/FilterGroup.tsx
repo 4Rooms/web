aaade6af5664ec08dd86d5f8fb85db7466db7d6f
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-10-23 20:15:53 +0300

    add

--- src/pages/Chats/PanelGroups/FilterGroup/FilterGroup.tsx
+++ src/pages/Chats/PanelGroups/FilterGroup/FilterGroup.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./FilterGroup.module.css";
 import { useChat } from "../../chat-context/use-chat";
 import { RowBelow } from "../../../../assets/icons";
@@ -6,8 +6,15 @@ import { NavLink, useLocation } from "react-router-dom";
 
 export default function FilterGroup() {
     const { roomName } = useChat();
+    const arrayCategory: string[] = ["New", "Popular", "Old"];
     const location = useLocation();
+    const [show, setShow] = useState<boolean>(false);
+    const [categotyChat, setCategotyChat] = useState<string>("New");
     const filterButton: string[] = ["Cinema", "Books", "Music", "Games"];
+    const changeNameCategory = arrayCategory.filter(
+        (category) => category !== categotyChat
+    );
+    console.log(changeNameCategory);
     return (
         <div>
             <ul className={styles.container__filterFroups}>
@@ -31,15 +38,37 @@ export default function FilterGroup() {
                     );
                 })}
             </ul>
-            <div>
+            <div className={styles.wrapper__new}>
                 <button
                     type="button"
+                    onClick={() => {
+                        setShow((prevState) => !prevState);
+                    }}
                     className={`${styles.button__new} ${
                         roomName ? styles[roomName] : ""
                     }`}
                 >
-                    New <RowBelow />
+                    {categotyChat} <RowBelow />
                 </button>
+                {show && (
+                    <ul className={styles.list__new}>
+                        {changeNameCategory.map((category: string) => (
+                            <li key={category} className={styles.item__new}>
+                                <button
+                                    onClick={(e: React.MouseEvent) => {
+                                        setCategotyChat(
+                                            (e.target as HTMLButtonElement)
+                                                .innerText
+                                        );
+                                    }}
+                                    className={styles.button__category_change}
+                                >
+                                    {category}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                )}
             </div>
         </div>
     );
