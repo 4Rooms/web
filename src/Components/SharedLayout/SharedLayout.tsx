57be87054d3e79c17fa9543bcbff8a5cb9e6676c
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2024-01-17 22:43:10 +0100

    start fix redirect to home, when we dont have token

--- src/Components/SharedLayout/SharedLayout.tsx
+++ src/Components/SharedLayout/SharedLayout.tsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 import styles from "./SharedLayout.module.css";
-import secureApi from "../../utils/axios-inteseptor/axios-interseptes.ts";
 import { AuthContext } from "../../pages/auth/signup-page/auth-context/auth-context.tsx";
 import { localStorageService } from "../../services/local-storage/local-storage.ts";
 import { useChat } from "../../pages/chats/chat-context/use-chat.tsx";
@@ -16,27 +15,12 @@ type Props = {
 
 export default function SharedLayout({ showHeader}: Props) {
     const location = useLocation();
-    const navigate = useNavigate();
     const { setChatOpen, setChatId } = useChat()
-    const { setUsername, setIsAuthenticated, setUserIcon } = useContext(AuthContext);
+    const { setUserIcon } = useContext(AuthContext);
     const is_verif = localStorageService?.get("user");
 
 
     useEffect(() => {
-        const queryParams = new URLSearchParams(location.search);
-        const token = queryParams.get('token');
-        if (token) {
-            const maxAge = 30 * 24 * 60 * 60;
-            document.cookie = `4roomToken=${token};path=/;max-age=${maxAge}`;
-            secureApi.get('user/').then((response) => {
-                setUsername(response.data.username);
-                localStorageService.set("user", response.data);
-                if (response.data.is_email_confirmed) {
-                    setIsAuthenticated(true);
-                }
-                navigate('/')
-            });
-        }
         authService.getProfile().then((response) => {
             setUserIcon(response.data.avatar);
         })
