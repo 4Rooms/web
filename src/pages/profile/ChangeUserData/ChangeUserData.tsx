3565db05c3cc9475f683c68ee3e2f44dd9243e69
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2024-01-23 15:14:36 +0100

    add

--- src/pages/profile/ChangeUserData/ChangeUserData.tsx
+++ src/pages/profile/ChangeUserData/ChangeUserData.tsx
@@ -199,6 +199,7 @@ export default function ChangeUserData() {
                             {formStateFocus[value] &&
                                 !formStateValid[value] &&
                                 !errors[value] &&
+                                //@ts-ignore
                                 formStateValue[value].length > 0 && (
                                     <div className={styles.focus}>
                                         <p>{t(`shared.${value}`)}</p>
